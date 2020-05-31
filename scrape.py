7515b986b3d59d80193a90a2a8f60e5cb4ccc819
Maliha Nan <malihanan09@gmail.com>
2020-05-31 21:37:14 +0530

    Modified - created a json list

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import json
 
 #for headless chrome
 options = Options()
@@ -13,11 +14,21 @@ driver.get("https://www.barchart.com/stocks/quotes/GOOG/competitors")
 #get all rows of the required table
 rows = driver.find_elements_by_xpath("//div[@class='bc-table-scrollable-inner']//tbody/tr")
 
-#get and print symbols and names which are in separate td elements
+companies = []
+
+#iterate through table rows
 for i,row in enumerate(rows):
-	symbol = row.find_element_by_xpath(".//td[@class='symbol text-left']//a").text
-	name = row.find_element_by_xpath(".//td[@class='symbolName text-left']//span[@data-ng-bind='cell']").text
-	print(symbol, "-", name)
+    #get symbols and names which are in separate td elements
+    symbol = row.find_element_by_xpath(".//td[@class='symbol text-left']//a").text
+    name = row.find_element_by_xpath(".//td[@class='symbolName text-left']//span[@data-ng-bind='cell']").text
+    #create a dict
+    company = {'Symbol': symbol, 'Name': name}
+    #append the dict in the list
+    companies.append(company)
+
+#dump it in a json list
+json_list = json.dumps(companies)
+print(json_list)
 
 #quit driver
 driver.quit()
