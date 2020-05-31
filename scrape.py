9d8fe6a864308cc90643fd16750ed961fd2adb89
Maliha Nan <malihanan09@gmail.com>
2020-05-31 21:13:43 +0530

    Modified - print symbols and names

--- scrape.py
+++ scrape.py
@@ -10,9 +10,14 @@ DRIVER_PATH = './chromedriver.exe'
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get("https://www.barchart.com/stocks/quotes/GOOG/competitors")
 
-#printing the title and url using WebDriver
-print(driver.title)
-print(driver.current_url)
+#get all rows of the required table
+rows = driver.find_elements_by_xpath("//div[@class='bc-table-scrollable-inner']//tbody/tr")
+
+#get and print symbols and names which are in separate td elements
+for i,row in enumerate(rows):
+	symbol = row.find_element_by_xpath(".//td[@class='symbol text-left']//a").text
+	name = row.find_element_by_xpath(".//td[@class='symbolName text-left']//span[@data-ng-bind='cell']").text
+	print(symbol, "-", name)
 
 #quit driver
 driver.quit()
